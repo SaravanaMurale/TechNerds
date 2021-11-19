50a56897e40223913e4c136af8aa86977dc66354
murali <murali.e@sosaley.in>
2021-11-19 22:51:09 +0530

    Initial commit

---
+++ app/src/main/java/com/pojo/technerds/MainActivity.java
@@ -0,0 +1,26 @@
+package com.pojo.technerds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        //MVVM
+        //Fragment
+        //Navigation Component
+        //ROOM
+        //Background Services
+        //Broadcast Receiver
+        //Push Notification
+
+    }
+}
