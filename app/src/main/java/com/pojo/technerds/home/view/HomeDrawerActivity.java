208e4133f2a4e48fdc9b9438786f4be5a086f298
murali <murali.e@sosaley.in>
2021-12-03 13:33:05 +0530

    init commit

---
+++ app/src/main/java/com/pojo/technerds/home/view/HomeDrawerActivity.java
@@ -0,0 +1,16 @@
+package com.pojo.technerds.home.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pojo.technerds.R;
+
+public class HomeDrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_drawer);
+    }
+}
